0589ff793973c90bb45b692da53674a534bf477b
chandan chanchanl <34713852+chandanchanchal@users.noreply.github.com>
2025-04-24 10:48:57 +0530

    Update practice.sql

--- practice.sql
+++ practice.sql
@@ -1,2 +1,7 @@
 sudo apt update -y
 sudo apt install mysql-server
+
+SELECT user, host, plugin FROM mysql.user WHERE user = 'root';
+
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Password123';
+FLUSH PRIVILEGES;
