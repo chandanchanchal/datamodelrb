4a87085d78c214d861609c14ebb2cfb206225603
chandan chanchanl <34713852+chandanchanchal@users.noreply.github.com>
2025-04-24 15:02:21 +0530

    Update join.sql

--- join.sql
+++ join.sql
@@ -9,3 +9,22 @@ FROM
     Customer c
 LEFT JOIN 
     Invoice i ON c.CustomerId = i.CustomerId;
+---------------------------------------------------------
+Expected Output:
+All customers will be listed.
+If a customer has an invoice, its InvoiceId and Total will appear.
+If not, InvoiceId and Total will be NULL.
+----------------------------------------------------------------------------
+2. INNER JOIN: Get Invoices with Their Customer Names
+To show only those invoices that are linked to existing customers.
+SQL Query:
+SELECT 
+    i.InvoiceId, i.InvoiceDate, c.FirstName || ' ' || c.LastName AS CustomerName, i.Total
+FROM 
+    Invoice i
+INNER JOIN 
+    Customer c ON i.CustomerId = c.CustomerId;
+---------------------------------------------------------
+Expected Output:
+Only invoices tied to a valid customer.
+No invoices with orphaned customer IDs.
