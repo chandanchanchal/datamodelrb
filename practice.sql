3bed2a281df793dd78cbec906723e9274df0701d
chandan chanchanl <34713852+chandanchanchal@users.noreply.github.com>
2025-04-24 10:55:28 +0530

    Update practice.sql

--- practice.sql
+++ practice.sql
@@ -7,3 +7,5 @@ SELECT user, host, plugin FROM mysql.user WHERE user = 'root';
 
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Password123';
 FLUSH PRIVILEGES;
+
+sudo snap install dbeaver-ce
