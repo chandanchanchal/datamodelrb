c4671246fc717c7a1e7273576d0ce8750773bb97
chandan chanchanl <34713852+chandanchanchal@users.noreply.github.com>
2025-04-24 16:13:32 +0530

    Update join.sql

--- join.sql
+++ join.sql
@@ -61,5 +61,11 @@ Entities(Table)& Relationships
 In the travel domain, key entities and their relationships can be defined as follows:
 Traveler: A person booking trips.
 Trip: Represents a journey taken by a traveler.
+Destination: Places where trips are made.
+Booking: A record of a trip booked by a traveler.
+Payment: Details of payments made for bookings.
+Transportation: Modes of transport (Flight, Train, Bus, etc.).
+Accommodation: Hotels or places travelers stay.
+Review: Ratings & feedback by travelers.
 
 
